fe0353c958f50235a001c2eb1efa05f703d27aaa
Emrys <emrys@local>
2025-04-03 09:22:51 +0200

    change for round system and som optimising names and code and fikse on spyglass

--- main.py
+++ main.py
@@ -2,42 +2,55 @@ import random
 
 
 class Dealer:
+    def __init__(self, actions):
+        self.actions = actions
+
     def steps(self, game):
         print("dealer turn")
         if game.bullet == "blank":
             print("Dealer: I will shot my self")
-            actions.shot(game)
+            self.actions.shot()
         elif game.bullet == "bang":
             print("Dealer: I will shot you")
-            actions.shot(game)
+            self.actions.shot()
+        game.turn = True
+
+        return True
 
 
 class Actions:
-    def shot(self, game):
+    def __init__(self, game):
+        self.game = game
+
+    def shot(self):
         who_to_hit = input("Dealer: who do you want to shot you or me ")
         if who_to_hit.lower() == "you" or "player":
-            game.healths["player"] -= game.round[game.bullet]
+            self.game.healths["player"] -= self.game.round_value[self.game.bullet]
         if who_to_hit.lower() == "me" or "dealer":
-            game.healths["dealer"] -= game.round[game.bullet]
+            self.game.healths["dealer"] -= self.game.round_value[self.game.bullet]
+        self.game.turn = True
 
-    def magnifying_glass(self, game):
-        print("bullet = ", game.bullet)
-        self.i -= 1
+    def spyglass(self):
+        print("bullet = ", self.game.bullet)
 
 
 class Game:
     def __init__(self, starting_health=3):
+        self.actions = actions = Actions(self)
+        self.dealer = Dealer(actions)
         self._healths = {
             "dealer": starting_health,
             "player": starting_health,
         }
         self.actions_name_map = {
-            "shot": Actions.shot,
+            "shot": self.actions.shot,
+            "spyglass": self.actions.spyglass,
         }
-        self.round = {"blank": 0, "bang": 1}
+        self.round_value = {"blank": 0, "bang": 1}
         self.shell = [
             random.choice(["blank", "bang"]) for _ in range(random.randint(6, 12))
         ]
+        self.turn = False
 
     @property
     def healths(self):
@@ -48,12 +61,14 @@ class Game:
         return self._healths == new_healths
 
     def get_user_input(self):
-        print("pleas whrite in camelcase")
         action = input(
             "Dealer: do you what to shot or a item? (write the name of the item you want to use)"
         )
-
-        self.actions_name_map[action](self)
+        try:
+            self.actions_name_map[action]()
+        except KeyError:
+            print("that is not a action")
+            return
 
     def count_bullets(self):
         bullets = 0
@@ -66,22 +81,28 @@ class Game:
 
         print(f" totale {bullets+blank}, bullets: {bullets}, blank: {blank}")
 
+    def round(self):
+        print(self.healths)
+        if (len(self.shell) - self.i) % 2 == 0:
+            self.turn = self.dealer.steps(self)
+        if not len(self.shell) % 2 == 0:  # or coin flip
+            self.get_user_input()
+        else:
+            RuntimeError("ingen sin turn!")
+        return False
+
     def steps(self):
         self.count_bullets()
         for self.i, self.bullet in enumerate(self.shell):
-            print("")
-            print(self.healths)
-            print("i =", self.i)
-            print((len(self.shell) - self.i) % 2 == 0)
-            if (len(self.shell) - self.i) % 2 == 0:
-                dealer.steps(game)
-            if not len(self.shell) % 2 == 0:  # or coin flip
-                self.get_user_input()
-            else:
-                RuntimeError("ingen sin turn!")
+            self.turn = False
+            while not self.turn:
+                print("while true")
+                self.turn = self.round()
 
 
-game = Game()
-actions = Actions()
-dealer = Dealer()
-game.steps()
+def main():
+    game = Game()
+    game.steps()
+
+
+main()
